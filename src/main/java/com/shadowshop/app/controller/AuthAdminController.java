2192c307e2a8ba3b24d9419b7817af0d5712802d
leeyoungseung <leeyoungseung@192.168.11.2>
2020-05-03 02:05:57 +0900

    create project and setting project

---
+++ src/main/java/com/shadowshop/app/controller/AuthAdminController.java
@@ -0,0 +1,22 @@
+package com.shadowshop.app.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthAdminController {
+
+	private static final Logger logger = LoggerFactory.getLogger(AuthAdminController.class);
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(Model model) {
+		logger.debug("Move to Login Page");
+		
+		return "/auth/login/loginForm";
+	}
+}
---
+++ src/main/java/com/shadowshop/app/controller/CategoryAdminController.java
@@ -0,0 +1,8 @@
+package com.shadowshop.app.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CategoryAdminController {
+
+}
